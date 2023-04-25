647c2a4ea1a102450ee9ddf74b56179e0b060d09
Jvicentemelo <52583833+Jvicentemelo@users.noreply.github.com>
2023-04-25 11:41:00 -0300

    Atualização tela Create

--- src/minhasEconomias/src/pages/Creat/index.tsx
+++ src/minhasEconomias/src/pages/Creat/index.tsx
@@ -1,119 +1,109 @@
-import React, {useEffect, useState} from 'react';
-import {Avatar} from 'react-native-elements';
-import {ScrollView, Text, View} from 'react-native';
+import React from 'react';
 
-import Input from '../../components/Input';
-import Button from '../../components/Button';
+import {useState} from 'react';
 
-import Style from '../Home/styles';
+import {View, StyleSheet} from 'react-native';
 
-import api from '../../services/axios';
-import AsyncStorage from '@react-native-community/async-storage';
-import {useNavigation} from '@react-navigation/native';
+import { Button, TextInput, Text } from 'react-native-paper';
 
-const Create = () => {
-    const {navigate} = useNavigation();
+
+
+
+
+function App() {
+
+ 
   
-    const [selectedOption, setSelectedOption] = useState(3);
-      
-    
-  
-    const handleOptionPress = (optionIndex: number) => {
-      setSelectedOption(optionIndex);
-    };
-  
-       
-  
-    return (
-      <View style={Style.container}>
-        <View style={Style.profileContainer}>
-          <Avatar
-            size="medium" // tamanho do Avatar
-            rounded // Avatar em formato de círculo
-            icon={{
-              name: 'user',
-              type: 'font-awesome',
-              size: 30,
-              color: '#444cb4',
-            }} // ícone do Avatar
-            onPress={() => navigate('EditProfile' as never)}
-            overlayContainerStyle={Style.avatarContainer} // cor de fundo do Avatar
-          />
-        </View>
-  
-        <View style={Style.optionsContainer}>
-          {options.map((option, index) => (
-            <TouchableOpacity
-              key={option.value}
-              style={[
-                options.length - 1 === index
-                  ? styles.optionButtonRadiusRight
-                  : index === 0
-                  ? styles.optionButtonRadiusLeft
-                  : styles.optionButtonRadiusCentrais,
-                selectedOption === index && styles.selectedOptionButton,
-              ]}
-              onPress={() => handleOptionPress(index)}>
-              <Text
-                style={[
-                  styles.optionButtonText,
-                  selectedOption === index && styles.selectedOptionButtonText,
-                ]}>
-                {option.label}
-              </Text>
-            </TouchableOpacity>
-          ))}
-          <EvilIcons size={50} name="calendar" />
-        </View>
-  
-        <View style={styles.amountContainer}>
-          <View style={styles.saldoContainer}>
-            <Text style={styles.balanceTitle}>{balanceTitle}</Text>
-          </View>
-          <View style={styles.balanceContainer}>
-            <Text style={styles.balanceValue}>{balanceValue}</Text>
-            <TouchableOpacity
-              style={styles.eyeIcon}
-              onPress={() => setShowBalance(!showBalance)}>
-              <FeatherIcons size={40} name={showBalance ? 'eye' : 'eye-off'} />
-            </TouchableOpacity>
-          </View>
-        </View>
-  
-        <View style={styles.line} />
-  
-        <ScrollView style={{maxHeight: 350, overflow: 'scroll'}}>
-          {expenses.map((expense, index) => {
-            return (
-              <ButtonContainer key={index} color={getButtonColor(index)}>
-                <ExpenseName color={getButtonColor(index)}>
-                  {expense.name}
-                </ExpenseName>
-                <ExpenseValue color={getButtonColor(index)}>
-                  {showBalance ? `RS ${expense.value.toFixed(2)}` : '****'}
-                </ExpenseValue>
-              </ButtonContainer>
-            );
-          })}
-        </ScrollView>
-  
-        <View style={styles.buttonsContainer}>
-          <Button
-            text="Adicionar"
-            color="purple"
-            halfSize={true}
-            onPress={handleCadastraDespesaouLucro}
-          />
-          <Button
-            text="Remover"
-            halfSize={true}
-            color="red"
-            onPress={handleRemoveDespesaouLucro}
-          />
-        </View>
-      </View>
-    );
+  const [text, onChangeText] = useState();
+
+ 
+
+  function InputText () {
+
+  return onChangeText(text);
+
   };
-  
-  export default Create;
-  
+
+ 
+
+  return (
+
+  <View style={styles.container}>
+
+  <Text style={styles.text}>Valor</Text>
+
+    <TextInput style={styles.input}
+
+    mode="outlined"
+
+    value={text}
+
+    />
+
+ 
+
+    <Text style={styles.text1}>Descrição</Text>
+
+    <TextInput
+
+    style={styles.input}
+
+    mode="outlined"
+
+    value={text}
+
+    />
+
+     
+
+  <Button style={styles.button} mode="contained" onPress={InputText}>
+
+    Adicionar
+
+  </Button>
+
+ 
+
+  </View>
+
+  );
+
+}
+
+export default App;
+
+ 
+
+const styles = StyleSheet.create({
+
+  text1: {
+
+    marginTop: 40,
+
+  },
+
+  container: {
+
+    marginTop: 120,
+
+    marginLeft: 20,
+
+    marginRight: 20
+
+  },
+
+  button: {
+
+    marginTop: 200,
+
+    backgroundColor: "#414BB2",
+
+  },
+  text: {
+
+  },
+  input: {
+
+  },
+
+});
