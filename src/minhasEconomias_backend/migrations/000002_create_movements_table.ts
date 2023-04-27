5b5116f04e6e8bf0ab9f55de3f158b22bb1f29cb
pedrovitorrs <pedro442.rodrigues@gmail.com>
2023-04-26 21:49:58 -0300

    fix: aplicando lint coding style.

--- src/minhasEconomias_backend/migrations/000002_create_movements_table.ts
+++ src/minhasEconomias_backend/migrations/000002_create_movements_table.ts
@@ -1,16 +1,17 @@
+/* eslint-disable no-unused-expressions */
 import { Knex } from 'knex'
 
 export async function up (knex: Knex): Promise<void> {
-    return knex.schema.createTable('movements', table => {
-      table.increments('id').primary()
-      table.string('description').notNullable
-      table.float('value').notNullable
-      table.date('date').notNullable
-      table.string('movementType').notNullable
-      table.integer('idUser').notNullable
-    })
-  }
-  
-  export async function down (knex: Knex): Promise<void> {
-    return knex.schema.dropTable('movements')
-  }
+  return knex.schema.createTable('movements', table => {
+    table.increments('id').primary()
+    table.string('description').notNullable
+    table.float('value').notNullable
+    table.date('date').notNullable
+    table.string('movementType').notNullable
+    table.integer('idUser').notNullable
+  })
+}
+
+export async function down (knex: Knex): Promise<void> {
+  return knex.schema.dropTable('movements')
+}
--- src/minhasEconomias_backend/src/controllers/MovementsController.ts
+++ src/minhasEconomias_backend/src/controllers/MovementsController.ts
@@ -2,36 +2,31 @@ import { Request, Response } from 'express'
 import knex from '../database/connection'
 
 export default class MovementsController {
-    public async create (req: Request, res: Response) {
-        const { description, value, date, movementType, idUser} = req.body
+  public async create (req: Request, res: Response) {
+    const { description, value, date, movementType, idUser } = req.body
 
-        const movement = {
-            description, 
-            value, 
-            date, 
-            movementType,
-            idUser
-        }
-
-        const findUser = await knex('users').where('id', idUser).select().first()
-
-
-        if (!findUser) {
-            return res.status(404).json({error: 'User not Found'})
-        }
-
-        const movementResponse = await knex('movements').insert(movement)
-
-        let id
-        
-        if (movementResponse.length > 0) {
-            id = movementResponse[0]
-        }
-
-        return res.json({id, ...movementResponse})
+    const movement = {
+      description,
+      value,
+      date,
+      movementType,
+      idUser
     }
 
+    const findUser = await knex('users').where('id', idUser).select().first()
+
+    if (!findUser) {
+      return res.status(404).json({ error: 'User not Found' })
+    }
+
+    const movementResponse = await knex('movements').insert(movement)
+
+    let id
+
+    if (movementResponse.length > 0) {
+      id = movementResponse[0]
+    }
+
+    return res.json({ id, ...movementResponse })
+  }
 }
-
-
-
