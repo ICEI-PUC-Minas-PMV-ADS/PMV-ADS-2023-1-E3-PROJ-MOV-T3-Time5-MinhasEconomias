4174245f6a4a8b4ffef6129fa94e6cbb22e0fb49
Karen Franco <karen_franco23@hotmail.com>
2023-04-26 21:04:30 -0300

    Endpoint para Criação de movimentação

--- src/minhasEconomias_backend/src/routes/index.ts
+++ src/minhasEconomias_backend/src/routes/index.ts
@@ -2,9 +2,11 @@ import { Router } from 'express'
 
 import SessionsController from '../controllers/SessionsController'
 import UsersController from '../controllers/UsersController'
+import MovementsController from '../controllers/MovementsController'
 
 const sessionsController = new SessionsController()
 const usersController = new UsersController()
+const movementsController = new MovementsController()
 
 const routes = Router()
 
@@ -13,5 +15,7 @@ routes.post('/sessions', sessionsController.authenticate)
 routes.post('/users', usersController.create)
 routes.get('/users/:id', usersController.show)
 routes.put('/users/:id', usersController.edit)
+
+routes.post('/movements', movementsController.create)
 
 export default routes
