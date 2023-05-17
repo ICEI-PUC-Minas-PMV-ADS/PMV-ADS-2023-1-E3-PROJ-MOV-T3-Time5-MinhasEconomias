6a549d5ce5d523cb5ab3b9eb125139f2280df067
Jvicentemelo <52583833+Jvicentemelo@users.noreply.github.com>
2023-05-17 16:08:32 -0300

    Merge Main
    Buscando atualizações da Main

--- src/minhasEconomias/src/pages/Transactions/Create/index.tsx
+++ src/minhasEconomias/src/pages/Transactions/Create/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React, {useState} from 'react';
 import {Avatar} from 'react-native-elements';
 import {Image, ScrollView, Text, TouchableOpacity, View} from 'react-native';
